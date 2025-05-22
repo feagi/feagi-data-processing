0ed90b15c656511b25ce4d54219fbd9673850053
Amir Rasteg <amir.rasteg@protonmail.com>
2025-05-22 18:55:13 -0400

    fix access

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ pub mod byte_data_functions;
 pub mod brain_input;
 pub mod brain_output;
 pub mod error;
-mod neuron_data;
-mod cortical_data;
+pub mod neuron_data;
+pub mod cortical_data;
 
 #[cfg(test)]
 mod tests {
