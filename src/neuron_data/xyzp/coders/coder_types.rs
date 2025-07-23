92e5ce655d598fa65da724b2bca9ab2b4bca06bf
Amir Rasteg <amir.rasteg@protonmail.com>
2025-07-23 15:17:23 -0400

    fix threading

--- src/neuron_data/xyzp/coders/coder_types.rs
+++ src/neuron_data/xyzp/coders/coder_types.rs
@@ -12,7 +12,7 @@ pub enum NeuronCoderVariantType {
 }
 
 pub fn instantiate_encoder_by_type(neuron_coder_type: NeuronCoderVariantType, cortical_ids_targeted: &[CorticalID], channel_dimensions: SingleChannelDimensions)
-    -> Result<Box<dyn NeuronXYZPEncoder>, FeagiDataProcessingError> {
+    -> Result<Box<dyn NeuronXYZPEncoder + Sync>, FeagiDataProcessingError> {
     
     const STANDARD_EXPECTED_CORTICAL_ID_COUNT: usize = 1;
     
